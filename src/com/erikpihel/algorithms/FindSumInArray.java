2c17c73529decbe953ecf76c78f1a8af22e910d7
epihel <erikpihel@yahoo.com>
2013-05-30 23:55:47 -0700

    Update FindSumInArray.java

--- src/com/erikpihel/algorithms/FindSumInArray.java
+++ src/com/erikpihel/algorithms/FindSumInArray.java
@@ -13,30 +13,29 @@ public class FindSumInArray {
 		// index of first element, the smallest number in first half of list
 		int firstHalfIndex = 0;
 		// index of last element, the largest number in second half of list
-	    int secondHalfIndex = arr.length - 1;
+		int secondHalfIndex = arr.length - 1;
 	    
 	    // loop until the middle of the array
 	    // when we've tried all additions
-	    while (firstHalfIndex < secondHalfIndex) {
-	    	int sum = arr[firstHalfIndex] + arr[secondHalfIndex];
-	    	//System.out.println("[" + arr[firstHalfIndex] + "] + [" + arr[secondHalfIndex] + "] = [" + sum + "]");
-	    	
-	    	if (sum == targetSum) {
-	    		return true;
-	    	}
-	    	
-	    	// we overshot: only way to make total smaller
-	    	// is to move one index down in the larger-number half
-	        else if (sum > targetSum) {
-	            --secondHalfIndex;
-	        }
-	        
-	    	// we undershot: only way to make total larger
-	    	// is to move one index up in the smaller-number half
-	        else {
-	        	++firstHalfIndex;
-	        }
-	    }
+		while (firstHalfIndex < secondHalfIndex) {
+			int sum = arr[firstHalfIndex] + arr[secondHalfIndex];
+			//System.out.println("[" + arr[firstHalfIndex] + "] + [" + arr[secondHalfIndex] + "] = [" + sum + "]");
+			if (sum == targetSum) {
+				return true;
+			}
+			
+			// we overshot: only way to make total smaller
+			// is to move one index down in the larger-number half
+			else if (sum > targetSum) {
+				--secondHalfIndex;
+			}
+			
+			// we undershot: only way to make total larger
+			// is to move one index up in the smaller-number half
+			else {
+				++firstHalfIndex;
+			}
+		}
 		
 		return false;
 	}
